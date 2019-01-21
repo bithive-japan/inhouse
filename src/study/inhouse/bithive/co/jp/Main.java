b019f1c7256188ef787b9bc5b867d139ceef2fa8
Takanori Asahara <asahara@bithive.co.jp>
2019-01-21 19:10:29 +0900

    new main

---
+++ src/study/inhouse/bithive/co/jp/Main.java
@@ -0,0 +1,10 @@
+package study.inhouse.bithive.co.jp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
